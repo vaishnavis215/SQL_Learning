8b95a7133580c2a5e48eeee06952b0f72b40c1f6
vaishnavis215 <vaishnavisuryawanshi703@gmail.com>
2025-09-28 21:11:54 +0530

    create a table

---
+++ Create.sql
@@ -0,0 +1,15 @@
+create table emp(
+emp_id int,
+f_name varchar(20),
+l_name varchar(20),
+salary float,
+address varchar(20),
+joining_date date,
+age tinyint,
+birth_year year,
+mobile_no varchar(30),
+gender enum('male','female','other'),
+project set('p-1','p-2','p-3','p-4')
+);
+create database test;
+
