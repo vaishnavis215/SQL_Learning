9414feb4c42d36202edee86dd0566c08737f6012
vaishnavis215 <vaishnavisuryawanshi703@gmail.com>
2025-09-28 21:17:10 +0530

    insert query

---
+++ insert query.sql
@@ -0,0 +1,8 @@
+insert into emp
+(emp_id,f_name,l_name,salary,address,joining_date,age,birth_year,mobile_no,gender,project) 
+values
+(1,'Radha','Suryawanshi',30000,'pune','2025-12-12',23,'2002',7057983628,'female','p-1');
+SELECT 
+    *
+FROM
+    emp;
